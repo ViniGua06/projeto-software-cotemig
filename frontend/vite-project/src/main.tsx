1e78e2aef872f480127a7aa88e691c4aa71f2562
Vinicius Guarnieri <viniguarnierisouza@gmail.com>
2024-05-12 13:08:03 -0300

    add redux-persistor

--- frontend/vite-project/src/main.tsx
+++ frontend/vite-project/src/main.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import { PersistGate } from "redux-persist/integration/react";
+
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { store, persistor } from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
