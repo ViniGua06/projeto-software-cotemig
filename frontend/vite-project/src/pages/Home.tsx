804cef2256f7c51462d12e5f7deb4ec94680e050
hugols <22200886@aluno.cotemig.com.br>
2024-05-17 08:17:20 -0300

    Landing Page Criada
    Criada landing page genérica.

--- frontend/vite-project/src/pages/Home.tsx
+++ frontend/vite-project/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "../components/Header";
+import { HomeMain } from "../components/HomeMain";
 
 const Home = () => {
   return (
     <>
       <Header></Header>
+      <HomeMain></HomeMain>
     </>
   );
 };
