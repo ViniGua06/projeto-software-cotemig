ea797e56316c9de936014a8608ccbdc0b5c3ddee
Vinicius Guarnieri Souza <viniguarnierisouza@gmail.com>
2024-05-20 08:57:56 -0300

    add change userpfp service on frontend

--- frontend/vite-project/src/components/Form/ChangePfpForm.tsx
+++ frontend/vite-project/src/components/Form/ChangePfpForm.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { select } from "../../redux/user/slice";
 
 import user_default from "../../assets/user_default.png";
+import { useState } from "react";
 
 const PfpForm = styled.form`
   display: flex;
@@ -16,14 +17,39 @@ const USerImg = styled.img`
   box-shadow: 0 0 20px black;
   width: 15%;
   border-radius: 50%;
+  height: 200px;
 `;
 
 export const ChangePfpForm = () => {
   const { user_pfp } = useSelector(select);
+  const [imagem, setImagem] = useState<any>("");
+
   return (
     <>
       <PfpForm>
         <USerImg src={user_pfp ? user_pfp : user_default}></USerImg>
+        <h1>Trocar Imagem de Perfil</h1>
+        <input
+          type="file"
+          accept=".png "
+          onChange={(e) => {
+            const files = e.target.files;
+
+            if (files && files?.length > 0) {
+              const file = files[0];
+
+              const reader = new FileReader();
+              reader.onload = () => {
+                setImagem(reader.result);
+              };
+
+              reader.readAsDataURL(file);
+            }
+          }}
+        />
+        <USerImg
+          src={imagem ? imagem : user_pfp ? user_pfp : user_default}
+        ></USerImg>
       </PfpForm>
     </>
   );
