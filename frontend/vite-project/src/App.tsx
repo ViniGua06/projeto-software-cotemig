debaf764cadbcc0a52f18abafaa7d92c863761e8
Vinicius Guarnieri Souza <viniguarnierisouza@gmail.com>
2024-05-27 08:24:26 -0300

    update

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./styles/home.css";
 
