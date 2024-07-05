20e8792cf8db787df0d40a438b0d81731fa2dab5
VINICIUS_GUARNIERI_SOUZA <22200460@OMEGAF08.FLORESTA>
2024-07-05 08:02:47 -0300

    test2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,13 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 2000;
 
-app.use(Cors());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-acess-token"],
+};
+
+app.use(Cors(corsOptions));
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
@@ -26,10 +32,10 @@ AppDataSource.initialize()
   .then(() => {
     const io = new Server(server, {
       cors: {
-        origin: "*", // Permitir solicitações de qualquer origem
-        methods: ["GET", "POST"], // Métodos permitidos
-        allowedHeaders: ["my-custom-header"], // Cabeçalhos personalizados permitidos
-        credentials: true, // Permitir credenciais (cookies, cabeçalhos de autorização, etc.)
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
       },
     });
 
