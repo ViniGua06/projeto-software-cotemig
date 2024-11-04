a5394603a4d1947c5304a660cf89a0a00ffbd789
shilde12 <22200886@aluno.cotemig.com.br>
2024-11-03 21:53:48 -0300

    Front-End Source Code Optimization
    - Removed header classes and IDs related to responsiveness
    - Removed header responsiveness (to be implemented in the future)
    - Removed comments
    - Removed duplicate classes with different attributes

--- frontend/vite-project/src/components/Header.tsx
+++ frontend/vite-project/src/components/Header.tsx
@@ -1,10 +1,10 @@
 import { Link, useNavigate } from "react-router-dom";
 import MenuHamb from "../sub-components/MenuHamb";
-import ResponsiveNav from "../components/ResponsiveNav";
 import { useSelector } from "react-redux";
 import { userSelect } from "../redux/user/slice";
 import { useState } from "react";
-import icon from "../assets/sistema_paroquias.svg";
+import icon from "../assets/sistema_paroquias.svg"
+import "../styles/home.css"
 
 const Icon = icon;
 
@@ -25,19 +25,12 @@ const Header = () => {
   return (
     <>
       <header>
-        <ul>
-          <Link to="/">
-            <img 
-              src={Icon}
-              width={"70px"}
-              alt=""
-            />
-          </Link>
-        </ul>
-
+        <Link to="/">
+          <img src={Icon} width={"70px"} alt="" />
+        </Link>
         <MenuHamb id="hamb" id_element="two"></MenuHamb>
       </header>
-      <ResponsiveNav></ResponsiveNav>
+      <div id="headerBreak"></div>
     </>
   );
 };
